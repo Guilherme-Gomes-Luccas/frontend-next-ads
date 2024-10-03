c9c6b3e596947c69851cf2be34358d929d860c84
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2024-10-02 22:16:20 -0300

    add react logo img

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Footer from "@/components/footer"
 import Header from "@/components/Header"
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -7,6 +8,12 @@ export default function Home() {
       <Header/>
       <main className="p-5">
         <h1 className="text-3xl">Titulo</h1>
+        <Image
+          src={'/react.png'}
+          width={300}
+          height={400}
+          alt="logo React"
+        />
         <p>texto muito texto</p>
         <p>texto muito texto</p>
       </main>
