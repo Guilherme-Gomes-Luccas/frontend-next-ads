80beddd861fad6062fa82d9c1354d13c56a8a638
Guilherme.Gomes.Luccas <guilhermegomes.luccas@gmail.com>
2024-10-24 09:56:15 -0300

    ajutes de estilo da pagina de conexão com o front

--- src/app/cadastro/page.jsx
+++ src/app/cadastro/page.jsx
@@ -8,17 +8,17 @@ import { useState } from "react";
 export default function Cadastro() {
   const [nome, setNome] = useState('')
   const [email, setEmail] = useState('')
-  const [senha, setPass] = useState('')
+  const [pass, setPass] = useState('')
 
   const handleSubmit = async () => {
-    console.log(nome, email, senha)
+    console.log(nome, email, pass)
 
     const response = await fetch('http://localhost:4000/user', {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({name: nome, email, senha})
+      body: JSON.stringify({name: nome, email, pass})
     })
 
     if (response?.ok) {
@@ -37,16 +37,22 @@ export default function Cadastro() {
         <h1 className="text-3xl">Cadastra-se</h1>
 
         <div className="flex-col gap-1 p-5 w-[300px]">
-          <label>Nome:</label>
-          <input type="text" className="border" value={nome} onChange={event => setNome(event.target.value)}/>
+          <div className="flex justify-between mb-2">
+            <label>Nome:</label>
+            <input type="text" className="border text-black w-[200px]" value={nome} onChange={event => setNome(event.target.value)}/>
+          </div>
 
-          <label>Email:</label>
-          <input type="text" className="border" value={email} onChange={event => setEmail(event.target.value)}/>
+          <div className="flex justify-between mb-2">
+            <label>Email:</label>
+            <input type="text" className="border text-black w-[200px]" value={email} onChange={event => setEmail(event.target.value)}/>
+          </div>
 
-          <label>Senha:</label>
-          <input type="text" className="border" value={senha} onChange={event => setPass(event.target.value)}/>
+          <div className="flex justify-between mb-2">
+            <label>Senha:</label>
+            <input type="text" className="borde text-black w-[200px]" value={pass} onChange={event => setPass(event.target.value)}/>
+          </div>
 
-          <button onClick={(handleSubmit)} className="border bg-slate-300 rounded">Cadastrar</button>
+          <button onClick={(handleSubmit)} className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded-full">Cadastrar</button>
         </div>
       </main>
       <Footer />
